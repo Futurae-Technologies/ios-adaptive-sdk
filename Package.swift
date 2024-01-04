eeb8037c0a2cc081de5439eff2f64647f86c8a7e
Valerii Hvozdiev <valerii@futurae.com>
2024-01-04 13:41:08 +0200

    Added v1.0.1-alpha release.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "AdaptiveKit",
     platforms: [
-        .iOS(.v9)
+        .iOS(.v12)
     ],
     products: [
         .library(name: "AdaptiveKit", targets: ["AdaptiveKit"])
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
           name: "AdaptiveKit",
-          url: "https://artifactory.futurae.com/artifactory/futurae-ios/AdaptiveKit-v1.0.0-alpha.xcframework.zip",
-          checksum: "85c7ac1ccfe27bdf21acd3ce97a3fed4c3ce0380d36628a0c1bfe66ac562963c"
+          url: "https://github.com/Futurae-Technologies/ios-adaptive-sdk/releases/download/v1.0.1-alpha/AdaptiveKit-v1.0.1.xcframework.zip",
+          checksum: "432956f7160743bdad739ee9703c12d9509229a79e450fbc6d9d31678ed9c729"
         )
     ]
 )
